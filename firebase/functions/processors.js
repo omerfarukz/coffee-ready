08946bd86a3cf122d72e934c7763f3a163018a80
Omer <omr@omrs-Mac-Pro.local>
2018-08-15 11:51:18 +0300

    Cleanup 5

--- firebase/functions/processors.js
+++ firebase/functions/processors.js
@@ -7,13 +7,13 @@ function dummyProcessor(before, after) {
 function readyToHaveProcessor(before, after) {
     if (before.state === 0 && after.state === 1) {
         const duration = Math.floor((after.at - before.at) / 60000.0); // minutes
-        if (duration > 3) {
+        if (duration > 3) { // is it over 3 minutes in power on state
             const content = duration > 0 ? "Cups: " + duration + "+ (beta)" : "";
             common.sendEmail("READY", content);
         }
     }
 }
-
+//
 const processors = [
     dummyProcessor      ,
     readyToHaveProcessor
