bb6cdc476a9f4c24ec3f56dd01584d563806f074
omerfarukz <ulrozremo@gmail.com>
2019-02-15 14:07:16 +0300

    added configuratin.sample.h for esp8266

---
+++ ESP8266/configuration.sample.h
@@ -0,0 +1,10 @@
+// firebase settings
+#define FIREBASE_HOST     "[project_id].firebaseio.com"
+#define FIREBASE_AUTH     "[auth_token]"
+
+// wifi settings
+#define WIFI_SSID         ""
+#define WIFI_PASSWORD     ""
+
+// device settings
+#define TRESHOLD          2.0
